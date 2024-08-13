14da6200757a67f997bbc25daaaaa40c92fb2199
joshborseth <joshuaborseth@gmail.com>
2024-08-13 14:00:31 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	let count = 0;
+</script>
+
+<main class="flex h-screen w-screen justify-center items-center flex-col gap-10">
+	<p class="text-3xl font-light">Count: {count}</p>
+	<h1 class="text-7xl font-bold">Hello World!</h1>
+	<Button size="lg" on:click={() => count++}>Click Me</Button>
+</main>
