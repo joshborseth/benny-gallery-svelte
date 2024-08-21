817096ecab3328c5bf1c0e8fcf74ebb81460d3e2
joshborseth <joshuaborseth@gmail.com>
2024-08-20 17:47:40 -0700

    push changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 </script>
 
+<nav class="absolute top-0 left-0">
+	<a href="/" class="text-blue-500 hover:text-blue-700">Home</a>
+</nav>
+
 <main class="max-w-3xl mx-auto min-h-screen flex items-center">
 	<slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,4 @@
 <script lang="ts">
-	import { Input } from '$lib/components/ui/input';
-	import Button from '@/components/ui/button/button.svelte';
-	import { blur } from 'svelte/transition';
-
-	let inputValue: string;
-	let show: boolean = false;
 </script>
 
-<div class="h-full w-full flex flex-col gap-2 justify-center items-center">
-	<h1 class="text-4xl font-bold">Welcome</h1>
-	{#if show}
-		<div class="w-full" in:blur={{ duration: 200 }} out:blur={{ duration: 200 }}>
-			<Input autofocus bind:value={inputValue} placeholder="Search..." />
-		</div>
-	{/if}
-	<Button on:click={() => (show = !show)}>Toggle</Button>
-</div>
+<div class="h-full w-full flex flex-col gap-2 justify-center items-center"></div>
